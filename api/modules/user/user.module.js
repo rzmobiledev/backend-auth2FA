fa24b58c498a95c108ca09d8a422f1a0e8c82609
safrizal <rzmobiledev@gmail.com>
2025-01-03 23:36:30 +0700

    modify vercel json and add api folder to gitignore

--- api/modules/user/user.module.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userService = void 0;
-const user_service_1 = require("./user.service");
-exports.userService = new user_service_1.UserService();
