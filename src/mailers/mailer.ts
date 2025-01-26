e2a01289e383803fe3a271fd24c3e08a1d1a7594
safrizal <rzmobiledev@gmail.com>
2025-01-26 09:34:16 +0700

    change mailer address to fe domain

--- src/mailers/mailer.ts
+++ src/mailers/mailer.ts
@@ -11,7 +11,7 @@ export type EmailFormatter = {
 }
 
 const mailer_sender = config.NODE_ENV === "production" ?
-    `no-reply <noreply@fe2fa.rzmobiledev.site>` : `no-reply <${config.MAILER_SENDER}>`
+    `no-reply <noreply@be2fe.rzmobiledev.site>` : `no-reply <${config.MAILER_SENDER}>`
 
 export const sendEmail = async <T extends EmailFormatter>(val: T): Promise<CreateEmailResponse> => {
     return await resend.emails.send({...val, from: mailer_sender})
