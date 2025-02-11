b7daca42c96312205ecd106b1ecf916b6043fd6d
safrizal <rzmobiledev@gmail.com>
2025-02-11 15:44:21 +0700

    change api to public

--- api/common/utils/uuid.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUniqueCode = generateUniqueCode;
-const uuid_1 = require("uuid");
-function generateUniqueCode() {
-    return (0, uuid_1.v4)().replace(/-/g, "").substring(0, 25);
-}
